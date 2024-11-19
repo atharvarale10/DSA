5a4a4023ebccc3b96cd0b61301a7400f2c5526dd
atharvarale10 <atharvasrale@gmail.com>
2024-11-19 14:22:59 +0530

    Create SearchLinkedList.c

---
+++ C/Searching/SearchLinkedList.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (!newNode) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+int searchElement(struct Node* head, int key) {
+    struct Node* current = head;
+    int position = 0;
+
+    while (current != NULL) {
+        if (current->data == key) {
+            return position;
+        }
+        current = current->next;
+        position++;
+    }
+    return -1; // Element not found
+}
+
+void displayList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtEnd(&head, 30);
+    insertAtEnd(&head, 40);
+
+    printf("Linked List: ");
+    displayList(head);
+
+    int key;
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int position = searchElement(head, key);
+
+    if (position != -1) {
+        printf("Element %d found at position %d\n", key, position);
+    } else {
+        printf("Element %d not found in the linked list\n", key);
+    }
+
+    return 0;
+}
